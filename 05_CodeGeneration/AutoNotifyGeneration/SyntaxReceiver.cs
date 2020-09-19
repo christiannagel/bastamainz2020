ebb11ba090e200235bd0924612ff76e27c81515d
Christian Nagel <christian@christiannagel.com>
2020-09-19 22:33:33 +0200

    code generation

---
+++ 05_CodeGeneration/AutoNotifyGeneration/SyntaxReceiver.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoNotifyGeneration
+{
+    /// <summary>
+    /// Created on demand before each generation pass
+    /// </summary>
+    class SyntaxReceiver : ISyntaxReceiver
+    {
+        public List<FieldDeclarationSyntax> CandidateFields { get; } = new List<FieldDeclarationSyntax>();
+
+        /// <summary>
+        /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
+        /// </summary>
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            // any field with at least one attribute is a candidate for property generation
+            if (syntaxNode is FieldDeclarationSyntax fieldDeclarationSyntax
+                && fieldDeclarationSyntax.AttributeLists.Count > 0)
+            {
+                CandidateFields.Add(fieldDeclarationSyntax);
+            }
+        }
+    }
+}
