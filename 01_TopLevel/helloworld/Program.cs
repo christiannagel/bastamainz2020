36f0bf6498c1a3d40f9fb2f996382eaf07553ad9
Christian Nagel <christian@christiannagel.com>
2020-09-21 21:14:46 +0200

    hello world top-level

---
+++ 01_TopLevel/helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+WriteLine("Hello, World!");
+ShowArgs(args);
+
+void ShowArgs(string[] args)
+{
+    foreach (var s in args)
+    {
+        WriteLine(s);
+    }
+}
